aecf0e2506a99e8d88f2b05fd844e06dd3214f99
shaikhaalbuflasa <shaikhasaad86@gmail.com>
2022-10-05 13:22:25 +0300

    medified 4

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 //get the html elements
-let tries = 7;
 let gameLetter = document.querySelector('.game-letter')
 let gameKeyboard = document.querySelector('.game-keyboard')
 let image = document.getElementById('game-img')
 let start = document.querySelector('.start-button')
+let reset = document.querySelector('.reset-button')
 
-//[div,div,div]
-
-//the words for the game
-let words= ['Rachel Karen Green','Monica Geller','Ross Geller','Ursula Buffay','Chandler Muriel Bing']
-
-start.addEventListener('click', startplay)
+// varibles 
+let tries = 6;
 let word;
 let text;
-function startplay (){
+let letter ;
+
+
+//the game array
+let words= ['Rachel Karen Green','Monica Geller','Ross Geller','Ursula Buffay','Chandler Muriel Bing']
+
+
+//make the button start cliklable
+// start.addEventListener('click', startthegame)
+
+//function for the click start button
+function startthegame (){
     image.style.backgroundImage = "url('img/guitar1.png')"
     word = words[Math.floor(Math.random() * words.length)];
     console.log(word);
     text = word.split(' ')
+    // console.log(text)
     text.forEach(function(name,index) {
         for (let i = 0; i < name.length; i++) {
           let newSpot = document.createElement('span')
@@ -33,106 +41,149 @@ function startplay (){
 }
 
 
-
-// console.log(text)
-
-  
-  
+//make the button keyboard key cliklable
 gameKeyboard.addEventListener('click', handleClick)
 
-    
-  
+//function for the click keyboard button
 function handleClick(event) {
     console.log(event.target.innerText)
     let userLetter = event.target.innerText;
     // if(text.includes(userLetter)){
-    // find the div that has the same index as that letter and change the innerHTML to match it\
-    let letter ;
-    tries--
+        
     // rmImage.toString();
-   if (tries == 0){
-    // console.log("hudhuhu")
-   // Get the modal
-            let modal = document.getElementById("myModal");
-
-            // Get the <span> element that closes the modal
-            const span = document.getElementsByClassName("close")[0];
-
-            // document.getElementById("myModal").style.display = "block";
-
-            modal.style.display = "block";
-            
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-            modal.style.display = "none";
-            }
-
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-            }
-   }else{
-    console.log("hds jdh")
-   }
     
-    // if (tries == 0){
-    //     console.log("game is over")
-    // }
-
+    
+    console.log('text,', text)
+    let totalLettersAdded
+    let lettersAdded = 0
     text.forEach(function(element, index) {
         //   console.log(element);
-            for (let i=0 ; i<element.length ; i++)  {
+        for (let i=0 ; i<element.length ; i++)  {
             letter = document.getElementById(`${index}-${i}`);
-        // console.log(text[i])
-        const pos = text.indexOf(letter);
-        if(pos < 0){
-        }
-            // rmImage()
-        if (element[i].toUpperCase() === userLetter.toUpperCase()){
-           letter.innerText = userLetter;
-           
-        }else{
+            // console.log(text[i])
+            // const pos = text.indexOf(letter);
+            // if(pos < 0){
+                // }
+                
+                if (element[i].toUpperCase() === userLetter.toUpperCase()){
+                    console.log()
+                    letter.innerText = userLetter;
+                    lettersAdded++
+                    totalLettersAdded++
+                    
+                }
+            }
+        })
+        
+        if (lettersAdded == 0){
+            tries--
+            if (tries < 0) {
+                tries =  0
+            }else if (tries == 0){
+                console.log("inside tries == 0")
+                // Get the modal
+                let modal = document.getElementById("myModal");
+
+            
+
+                    modal.style.display = "block";
+                  
+
+                
+                // Get the <span> element that closes the modal
+                const span = document.getElementsByClassName("close")[0];
+                
+                // document.getElementById("myModal").style.display = "block";
+                
+                
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function() {
+                    modal.style.display = "none";
+                }
+                
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+            }else{
+            console.log("You have this many chances left: " + tries)
+            }
             rmImage(tries)
             
         }
-      }
-    })
-}
-      
+        
+     // if (tries == 0){
+        //         console.log("inside tries == 0")
+        //         // Get the modal
+        //         let modal = document.getElementById("myModal");
+                
+        //         // Get the <span> element that closes the modal
+        //         const span = document.getElementsByClassName("close")[0];
+                
+        //         // document.getElementById("myModal").style.display = "block";
+                
+        //         modal.style.display = "block";
+                
+        //         // When the user clicks on <span> (x), close the modal
+        //         span.onclick = function() {
+        //             modal.style.display = "none";
+        //         }
+                
+        //         // When the user clicks anywhere outside of the modal, close it
+        //         window.onclick = function(event) {
+        //             if (event.target == modal) {
+        //                 modal.style.display = "none";
+        //             }
+        //         }
+        // }else{
+        //     console.log("You have this many chances left: " + tries)
+        // }
+        //if (totalLettersAdded == // total length of the name)
+        // user wins
+    }  
     
-function rmImage(tries){
-    console.log('tries',tries)
-    switch(tries.toString()){
-        case "5":
-            image.style.backgroundImage = "url('img/guitar2.png')";
-        break;
-        case "4":
-            image.style.backgroundImage = "url('img/guitar3.png')";
-        break;
-        case "3":
-            image.style.backgroundImage = "url('img/guitar4.png')";
-        break;
-        case "2":
-            image.style.backgroundImage = "url('img/guitar5.png')";
+    //function for the imag switch basedon the tries numbers
+    function rmImage(tries){
+        console.log('tries',tries)
+        switch(tries.toString()){
+            case "5":
+                image.style.backgroundImage = "url('img/guitar2.png')";
+                break;
+                case "4":
+                    image.style.backgroundImage = "url('img/guitar3.png')";
+                    break;
+                    case "3":
+                        image.style.backgroundImage = "url('img/guitar4.png')";
+                        break;
+                        case "2":
+        image.style.backgroundImage = "url('img/guitar5.png')";
         break;
         case "1":
-            image.style.backgroundImage = "url('img/guitar6.png')";
+        image.style.backgroundImage = "url('img/guitar6.png')";
         break;
         case "0":
-            image.style.backgroundImage = "url('img/guitar7.png')";
+        image.style.backgroundImage = "url('img/guitar7.png')";
         break;
         // default:
-        //     image.style.backgroundImage = "url('img/guitar1.png')";
-        // break;
-    }
+       //     image.style.backgroundImage = "url('img/guitar1.png')";
+       // break;
+    }  
 };
+                             
 
 
-
-
-
+//make the button reset cliklable                               
+reset.addEventListener('click', resetthegame)
+                                
+ //function for the click reset button                               
+function resetthegame (){
+ gameLetter.innerHTML = ''
+ tries=6;
+  startthegame()
+}
+                                
 
 
 
