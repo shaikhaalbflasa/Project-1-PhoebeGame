b67f29104eb490a356a9bc83d6bc29cd72aa9da1
shaikhaalbuflasa <shaikhasaad86@gmail.com>
2022-10-12 03:50:58 +0300

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# Project-1-PhoebeGame
-https://shaikhaalbflasa.github.io/Project-1-PhoebeGame/
+Project-1-PhoebeGame
+======
+
+Game name
+============
+Phoebe Buffay Game
+
+GAME Description
+==================
+ Phoebe Buffay game is a guessing game for one player.  The word of the game is take from friends TV show ,such as the characters. The player has to guess a within six tries or or until the word is completed,
+ each time the player choose the wrong letter the Phoebe guitar strings will to be cut off. There is will a hint to helpe the player guess the word. At the end of a game, the player is asked whether they want to play again or return to the home page. 
+
+Live Site
+====================
+[Phoebe Buffay Game link](https://shaikhaalbflasa.github.io/Project-1-PhoebeGame/)
+
+Technology used
+=================
+- HTML,CSS,Javascrip 
+- Dom Manipulation 
+
+
+Wireframes
+=======
+- Wireframe 1
+
+<img width="1439" alt="Wireframe 1" src="https://user-images.githubusercontent.com/113825270/195223686-067596b7-dc66-4db3-b7d1-7707801c13c7.png">
+
+- Wireframe 2
+
+<img width="1436" alt="Wireframe 2" src="https://user-images.githubusercontent.com/113825270/195223826-690653bf-fa56-4394-92cb-ce10995a7627.png">
+
+- Wireframe 3
+
+<img width="1435" alt="Wireframe 3" src="https://user-images.githubusercontent.com/113825270/195223930-d961304f-9523-40fe-8315-56517a2cd222.png">
+
+- Wireframe 4
+
+<img width="1440" alt="Wireframe 4" src="https://user-images.githubusercontent.com/113825270/195224015-07a2cd74-cc12-4a6c-beca-1d89d84305c4.png">
+
+
+
+
+
+Sources
+======
+- W3schools
+- Developer mozilla
+- Google Fonts
+- Figma
+
+
+
+
+ 
+
+
+
+
--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let gameKeyboard = document.querySelector('.game-keyboard')
 let key = document.querySelector('.key')
 let image = document.getElementById('game-img')
 let start = document.querySelector('.start-button')
-let reset = document.querySelector('.reset-button')
+let resetbutton1 = document.querySelector('.reset-button1')
+let resetbutton2 = document.querySelector('.reset-button2')
 let remaintesies = document.querySelector('.teries-remain')
 let gameHint = document.querySelector('.game-hint')
 let buttons = document.querySelectorAll('.key')
 
-buttons.forEach(btn => btn.addEventListener('click', handleClick))
 
 // varibles 
 let tries = 6;
@@ -17,27 +17,24 @@ let word;
 let text;
 let letter ;
 let totalLettersAdded = 0
-let sound= new Audio('mixkit-short-guitar-strum-2318.wav')
+
+let sound= new Audio('sound/mixkit-short-guitar-strum-2318.wav')
 
 
-
-
-// function for the ininstructions button
+// ---function for the ininstructions button-------
 function instructions(){
     document.getElementById("myModalforinstructions").style.display="block"
 }
-
 function closetheinstructions(){ 
-   document.getElementById("myModalforinstructions").style.display="none"  
+    document.getElementById("myModalforinstructions").style.display="none"  
 }
-    
-   
-//the game array
+
+//-----the game obj-----
 let words = [
     {hint: 'Characters Starts with R', name: 'Rachel Karen Green'},
     {hint: 'Characters Starts with M', name: 'Monica Geller'},
-    {hint: 'Characters Starts with p ', name: 'Phoebe Buffay'},
-    {hint: 'Characters Starts with u ', name: 'Ursula Buffay'},
+    {hint: 'Characters Starts with P ', name: 'Phoebe Buffay'},
+    {hint: 'Characters Starts with U ', name: 'Ursula Buffay'},
     {hint: 'Characters Starts with J ', name: 'Joey Tribbiani'},
     {hint: 'Characters Starts with C ', name: 'Chandler Muriel Bing'},
     {hint: 'Characters Starts with G', name: 'Gina Tribbiani'},
@@ -46,18 +43,21 @@ let words = [
     {hint: 'Characters Starts with Z ', name: 'Zach Miller'},
     {hint: 'Characters Starts with M ', name: 'Michael Tribbiani'},
 ]
-// console.log("this"+ words)
 
-//function for the click start button
+//-------function for the click start button------
 function startthegame (){
+    //show the imag when start the game
     image.style.backgroundImage = "url('img/guitar1.png')"
+    //randon the obj 
     let randNum = Math.floor(Math.random() * words.length) 
+    //take the name only from the random obj
     word = words[randNum].name;
-    // console.log(word);
+    console.log(word);
     gameHint.innerHTML="Hint: "+ words[randNum].hint
 
     text = word.split(' ')
-    console.log(text)
+    // console.log(text)
+    //make the -- for the use click letter
     text.forEach(function(name,index) {
         for (let i = 0; i < name.length; i++) {
             let newSpot = document.createElement('span')
@@ -65,81 +65,71 @@ function startthegame (){
             newSpot.innerHTML = ' _ '
             gameLetter.appendChild(newSpot)
         }
+        //for the space btween 2 words
         let emptySpot = document.createElement('span')
         emptySpot.innerHTML = '&nbsp;&nbsp;'
         gameLetter.appendChild(emptySpot)
     })
 }
 
-//make the button keyboard key cliklable
-// gameKeyboard.addEventListener('click', handleClick)
+//add addEventListener click to the buttons
+buttons.forEach(btn => btn.addEventListener('click', handleClick))
 
-//function for the click keyboard button
+//------function for the click keyboard button------
 function handleClick(event) {
     // console.log(event.target.innerText)
- 
     let userLetter = event.target.innerText;
     event.target.style.backgroundImage = "url('img/guitar1.png')";
     
-
-
-    
-    let numberpush=[]
-   
-    if (numberpush.includes(userLetter)){
-        return 
-    }
-    
-    
-   
     // console.log('text,', text)
     let lettersAdded = 0;
     text.forEach(function(element, index) {
         //   console.log(element);
         for (let i=0 ; i<element.length ; i++)  {
+            //to compere between the id  for the - and the use letter
             letter = document.getElementById(`${index}-${i}`);
             
-           
+           //to check it the same in the text 
             if (element[i].toUpperCase() === userLetter.toUpperCase()){
                 letter.innerText = userLetter;
                 lettersAdded++
                 totalLettersAdded++
-                numberpush.push(userLetter)
-
-
-                
+                // numberpush.push(userLetter)
             }  
-           
-            
         }
         })
        
         if (lettersAdded == 0){
-            
-            
             tries--
             if (tries < 0) {
                 tries =  0
-                
-              
+
             }else if (tries == 0){
-               
-                // console.log("inside tries == 0")
                 // Get the modal
                 let modal2 = document.getElementById("lossModal");
-
                 modal2.style.display = "block";
                 // Get the <span> element that closes the modal
                 const span = document.getElementsByClassName("close2")[0];
 
-                
-                
+
+                //When the user clicks reset button
+                resetbutton1.onclick=function(){
+                    gameLetter.innerHTML = ''
+                    gameHint.innerHTML= ' '
+                   remaintesies.innerText= ''
+                    tries = 6;
+                   totalLettersAdded = 0;
+                   for(let i = 0 ; i < document.getElementsByTagName('button').length ; i++){
+                    document.getElementsByTagName('button')[i].style = '';
+                   }
+                  startthegame()
+    
+                }
                 
                 // When the user clicks on <span> (x), close the modal
                 span.onclick = function() {
                     modal2.style.display = "none";
                 }
-                
                 // When the user clicks anywhere outside of the modal, close it
                 window.onclick = function(event) {
                     if (event.target == modal2) {
@@ -153,23 +143,20 @@ function handleClick(event) {
             rmImage(tries)
             
         }
+        // console.log("totallettersadded " + totalLettersAdded)
+        // console.log("total "+ text.join("").length)
 
-        
-        console.log("totallettersadded " + totalLettersAdded)
-        console.log("total "+ text.join("").length)
+        //to check if the user inerr same as the leght of the text it win and i remove the spit from tha text
      if (totalLettersAdded == text.join("").length){
-            // console.log("Im here 2 ")
-
+        
             // Get the modal
             let modal3 = document.getElementById("winModal");
-
             modal3.style.display = "block";
             // Get the <span> element that closes the modal
             const span = document.getElementsByClassName("close3")[0];
 
-            let resetbutton2 = document.querySelector('.reset-button2')
-            
-           resetbutton2.onclick=function(){
+            //When the user clicks reset button
+            resetbutton2.onclick=function(){
                 gameLetter.innerHTML = ''
                 gameHint.innerHTML= ' '
                remaintesies.innerText= ''
@@ -181,8 +168,6 @@ function handleClick(event) {
               startthegame()
 
             }
-            
-            
             // When the user clicks on <span> (x), close the modal
             span.onclick = function() {
                 modal3.style.display = "none";
@@ -197,13 +182,13 @@ function handleClick(event) {
 
     }
 
-    
-  
-     event.target.removeEventListener('click', handleClick)
-         
-       
-}     
-//function for the imag switch basedon the tries numbers
+  /// to click ones only 
+    event.target.removeEventListener('click', handleClick)
+            
+}  
+
+
+//----function for the imag switch basedon the tries numbers-----
 function rmImage(tries){
         // console.log('tries',tries)
         switch(tries.toString()){
@@ -237,21 +222,6 @@ function rmImage(tries){
     }  
 };
                             
-//make the button reset cliklable                               
-reset.addEventListener('click', resetthegame)                               
-//function for the click reset button                               
-function resetthegame (){
- gameLetter.innerHTML = ''
- gameHint.innerHTML= ' '
- remaintesies.innerText= ''
- tries = 6;
- totalLettersAdded = 0;
-  for(let i = 0 ; i < document.getElementsByTagName('button').length ; i++){
-    document.getElementsByTagName('button')[i].style = '';
-  }
-  startthegame()
-}
-                                
 
 
 
